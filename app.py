7b01f243ce3836ca6ff82fad60ff54d95933154e
Yousef.Salah <khodrojy3@gmail.com>
2023-12-13 18:02:34 +0200

    re-format code

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-
 import json
-import spacy
-
-
 from typing import Any
-from bs4 import BeautifulSoup,SoupStrainer
+
+import spacy
+from bs4 import BeautifulSoup, SoupStrainer
 from geopy.geocoders import Nominatim
 
 """
@@ -14,12 +12,12 @@ from geopy.geocoders import Nominatim
         PLACES: multi place liste in which each place is surrounded by <D>
 
 """
-nlp = spacy.load('en_core_web_lg')
+nlp = spacy.load("en_core_web_lg")
 
-f = open('data/reut2-000.sgm', 'r')
-data= f.read()
+f = open("data/reut2-000.sgm", "r")
+data = f.read()
 soup = BeautifulSoup(data, features="html.parser")
-contents = soup.findAll(str.lower('REUTERS'))
+contents = soup.findAll(str.lower("REUTERS"))
 geolocator = Nominatim(user_agent="smart")
 
 
@@ -33,22 +31,20 @@ def extract_text(element: Any) -> str:
 records = []
 counter = 1
 for content in contents:
-
-
     string_converter = lambda x: x.get_text()
 
-    date = extract_text(content.find('date'))
-    topics = list(map(string_converter, content.find('topics').findAll('d')))
-    places = list(map(string_converter, content.find('places').findAll('d')))
-    people = list(map(string_converter, content.find('people').findAll('d')))
-    orgs = list(map(string_converter, content.find('orgs').findAll('d')))
-    exchanges = list(map(string_converter, content.find('exchanges').findAll('d')))
-    companies = list(map(string_converter, content.find('companies').findAll('d')))
-    title = extract_text(content.find('text').find('title'))
-    body = extract_text(content.find('text').find('body'))
-    dateline = extract_text(content.find('text').find('dateline'))
+    date = extract_text(content.find("date"))
+    topics = list(map(string_converter, content.find("topics").findAll("d")))
+    places = list(map(string_converter, content.find("places").findAll("d")))
+    people = list(map(string_converter, content.find("people").findAll("d")))
+    orgs = list(map(string_converter, content.find("orgs").findAll("d")))
+    exchanges = list(map(string_converter, content.find("exchanges").findAll("d")))
+    companies = list(map(string_converter, content.find("companies").findAll("d")))
+    title = extract_text(content.find("text").find("title"))
+    body = extract_text(content.find("text").find("body"))
+    dateline = extract_text(content.find("text").find("dateline"))
     doc = nlp(dateline)
-    locations = [ent.text for ent in doc.ents if ent.label_ == 'GPE']
+    locations = [ent.text for ent in doc.ents if ent.label_ == "GPE"]
     location = locations[0] if len(locations) else ""
     longitude = ""
     latitude = ""
@@ -79,5 +75,5 @@ for content in contents:
     print(counter)
     counter += 1
 
-with open('data.json', 'w', encoding='utf-8') as f:
+with open("data.json", "w", encoding="utf-8") as f:
     json.dump(records, f)
